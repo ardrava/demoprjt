b144fd84a1a3fdb86c7abb0802db818febf86b21
ardrava <98315389+ardrava@users.noreply.github.com>
2023-05-30 22:57:14 +0530

    Update config.php

--- code/config.php
+++ code/config.php
@@ -1,1 +1,13 @@
+<?php
+    $url='localhost';
+    $username='root';
+    $password='';
+    $conn=mysqli_connect($url,$username,$password,"ais");
+    if(!$conn){
+        die('Could not Connect My Sql:' .mysql_error());
+    }
+    
+?>
+
+
 
